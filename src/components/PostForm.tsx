672a8cacbdb4ad3d2681f6b9c0e99a4bfa696a24
StanislavKaisin <8050361@gmail.com>
2021-03-14 12:51:10 +0200

    base layout created

---
+++ src/components/PostForm.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class PostForm extends Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>PostForm</div>;
+  }
+}
